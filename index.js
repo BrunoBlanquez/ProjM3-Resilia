74ab08a75d3c8430c255d3d9d9740c61a74fdd75
Matric Bastos <matricbts@gmail.com>
2022-03-26 17:56:59 -0300

    ajustes pesquisa

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 
-const botao = $('#btn')
+const botao = $('#btn');
+const painelDeJogos = $('#painelDeJogos');
+const inputJogo = $('#nomeJogo');
+const headerJogo = $('#headerJogo')
 // const jogoDaPesquisa = $('#inputPesquisa')
 
-botao.on('click', async function() {  
-    try {
-        const response = await fetch('http://localhost:4567/');
-        const data = await response.json();
-        console.log(data);
-        
-    } catch (error) {
-        console.error('erro : ', error);
-    }
+// botao.on('click', async function() {  
+//     buscaTabela();
+// })
+
+
+botao.on('click', function name(params) {
+    let teste = '';
+    const tabela = buscaTabela(teste); // then é pra pegar o valor da promise
+    
+    console.log('testando retorno da função', tabela);
 })
 
+async function buscaTabela(teste) {
+    try {
+        const response = await fetch('http://localhost:4567/'); 
+        //return teste;   
+
+        
+        const data = await response.json();
+        console.log()
+        console.log(data);
+        inputJogo.val(data.short_description);
+        console.log(inputJogo.val())
+
+        headerJogo.attr('src', data.header_image);
+
+        painelDeJogos.append(data[0]);
+            // return data;
+        }
+        //return data;
+    catch (error) {
+        console.error('erro : ', error);
+    }
+}
--- src/controllers/consulta.js
+++ src/controllers/consulta.js
@@ -6,10 +6,11 @@ app.use(cors());
 
 app.get('/', (req, res) => {
     
+    // SALVANDO O RETORNO DA QUERY EM UMA VARIAVEL
     async function testando(){
         const db = require('./../database/db');
         //console.log('select * from steam');
-        const listaDeJogos =  await db.selectGames();
+        const listaDeJogos =  await db.selectDescription();
         return res.json( listaDeJogos[0][0] )
     
     };
