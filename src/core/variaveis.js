41c29b5335c26f67370b4dec9c7409aedf9cc07e
Matric Bastos <matricbts@gmail.com>
2022-03-30 02:12:26 -0300

    melhorando arquitetura

--- src/core/variaveis.js
+++ src/core/variaveis.js
@@ -1,12 +1,12 @@
-const painelDeJogos = $('#painelDeJogos');
 const inputJogo = $('#nomeJogo');
 const headerJogo = $('#headerJogo');
 const tituloJogo = $('#tituloJogo');
 const descricaoJogo = $('#descricaoJogo');
-
 const imagemScreenshot = $('#imagemScreenshot');
 const analisesPositivas = $('#analisesPositivas');
 const dataLançamento = $('#dataLançamento');
 const desenvolvedor = $('#desenvolvedor');
 const distribuidora = $('#distribuidora');
+const apiUrl = 'http://localhost:3000/';
 
+export {inputJogo, headerJogo, tituloJogo, descricaoJogo, imagemScreenshot, analisesPositivas, dataLançamento, desenvolvedor, distribuidora, apiUrl}
