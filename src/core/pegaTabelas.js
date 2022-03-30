e05361645b7302dc8b9b6d28ba6ad25fac2a59a4
Matric Bastos <matricbts@gmail.com>
2022-03-30 01:42:59 -0300

    melhorando consulta no api com apenas uma função

--- src/core/pegaTabelas.js
+++ src/core/pegaTabelas.js
@@ -3,93 +3,71 @@ const inputJogo = $('#nomeJogo');
 const headerJogo = $('#headerJogo');
 const tituloJogo = $('#tituloJogo');
 const descricaoJogo = $('#descricaoJogo');
-
 const imagemScreenshot = $('#imagemScreenshot');
 const analisesPositivas = $('#analisesPositivas');
 const dataLançamento = $('#dataLançamento');
 const desenvolvedor = $('#desenvolvedor');
 const distribuidora = $('#distribuidora');
+const apiUrl = 'http://localhost:3000/';
 
 
 //TABELA COMPLETA DA STEAM
 
-async function buscaTabela() {
-    try {
-            const response = await fetch(`http://localhost:3000/descricao/${inputJogo.val()}`); 
-            const data = await response.json();
-            console.log(data);
+async function buscaTabela() {  
+    const data = await consultaApi(`descricao/${inputJogo.val()}`);
+    console.log(data);
 
-            $('.informacoesJogo').css('display', 'flex')
-            tituloJogo.html(data[0][0].name);
-            headerJogo.attr('src', data[0][0].header_image);
-            descricaoJogo.html(data[0][0].short_description);
+    $('.informacoesJogo').css('display', 'flex')
+    tituloJogo.html(data[0][0].name);
+    headerJogo.attr('src', data[0][0].header_image);
+    descricaoJogo.html(data[0][0].short_description);
 
-            let screenshotsArray = Array.from(data[0][0].screenshots); // TRANSFORMA A INFO EM ARRAY
-            let arrayFiltro = screenshotsArray.filter( letra => letra !== '[' ).filter(letra => letra !== ']');// TIRA STRING QUE SÃO COLCHETES
-            let objeto = arrayFiltro.join('').split(',')[4]; // PEGA A INFO DA SCREEN QUE TA NA POSIÇÃO 4
-            let finalRetorno = objeto.replace("'path_thumbnail': ",'').replace("'",''); // TIRA TODOS OS CARACTERES DEIXANDO APENAS O LINK DA SCREENSHOT
+    // TRATANDO URL DO SCREENSHOT
+    let screenshotsArray = Array.from(data[0][0].screenshots); // TRANSFORMA A INFO EM ARRAY
+    let arrayFiltro = screenshotsArray.filter( letra => letra !== '[' ).filter(letra => letra !== ']');// TIRA STRING QUE SÃO COLCHETES
+    let objeto = arrayFiltro.join('').split(',')[4]; // PEGA A INFO DA SCREEN QUE TA NA POSIÇÃO 4
+    let finalRetorno = objeto.replace("'path_thumbnail': ",'').replace("'",''); // TIRA TODOS OS CARACTERES DEIXANDO APENAS O LINK DA SCREENSHOT
 
-            
-            imagemScreenshot.attr('src', finalRetorno);
-            analisesPositivas.html(`POSITIVE RATINGS: ${data[0][0].positive_ratings}`);
-            desenvolvedor.html(`DEVELOPER: ${data[0][0].developer}`);
-            distribuidora.html(`PLUBISHER: ${data[0][0].publisher}`);
-            dataLançamento.html(`DATA LANÇAMENTO: ${data[0][0].release_date}`)
+    imagemScreenshot.attr('src', finalRetorno);
 
-            inputJogo.val('')
-            
-        }
+    analisesPositivas.html(`POSITIVE RATINGS: ${data[0][0].positive_ratings}`);
+    desenvolvedor.html(`DEVELOPER: ${data[0][0].developer}`);
+    distribuidora.html(`PLUBISHER: ${data[0][0].publisher}`);
+    dataLançamento.html(`DATA LANÇAMENTO: ${data[0][0].release_date}`)
 
-    catch (error) {
-        console.error('erro : ', error);
-    }
+    inputJogo.val('')
+
 }
 
-
 // BUSCANDO TOP5
-async function pegaTop5(params) {
-    try {
-        const response = await fetch('http://localhost:3000/top5gratis'); 
-        const data = await response.json();
-        console.log(data);
+async function pegaTop5() {
+    const data = await consultaApi('top5gratis');
 
-        for (let i = 0; i < 5; i++) {
-            $('#divTop5').append(`<div id="${data[0][i].name}"  class="fileira">
-                <img id="imgJogoGratis0" src="${data[0][i].header_image}" alt="">
-                <p id="tituloJogoGratis0">${data[0][i].name}</p>
-            </div>`)
-        }         
-    }
-    catch (error) {
-        console.error('erro : ', error);
-    }
+    for (let i = 0; i < 5; i++) {
+        $('#divTop5').append(`<div id="${data[0][i].name}"  class="fileira">
+            <img id="imgJogoGratis0" src="${data[0][i].header_image}" alt="">
+            <p id="tituloJogoGratis0">${data[0][i].name}</p>
+        </div>`)
+    }         
 }
 
 // BUSCANDO LANÇAMENTOS
 async function pegaLancamentos() {
-    try {
-        const response = await fetch('http://localhost:3000/lancamentos');
-        const data = await response.json();
+    const data = await consultaApi('lancamentos');
 
-        for (let i = 0; i < 5; i++) {
-            $('#divLancamentos').append(`<div class="fileira">
-                <img id="imgJogoGratis0" src="${data[0][i].header_image}" alt="">
-                <p id="tituloJogoGratis0">${data[0][i].name}</p>
-            </div>`)
-        }
-    } catch (error) {
-        console.error('erro : ', error);
-    }
+    for (let i = 0; i < 5; i++) {
+        $('#divLancamentos').append(`<div class="fileira">
+            <img id="imgJogoGratis0" src="${data[0][i].header_image}" alt="">
+            <p id="tituloJogoGratis0">${data[0][i].name}</p>
+        </div>`)
+    } 
 }
 
 //TABELA FILTRADA
 async function pegaFiltro(valor) {
-    try {
-        const response = await fetch(`http://localhost:3000/filtrovalor/${valor}`);
-        const data = await response.json();
+        const data = await consultaApi(`filtrovalor/${valor}`);
 
         for (let i = 0; i < 5; i++) {
-
             if (data[0][i]) {
                 $(`#imgFiltro${i}`).attr('src', data[0][i].header_image);
                 $(`#tituloFiltro${i}`).html(data[0][i].name);
@@ -101,25 +79,27 @@ async function pegaFiltro(valor) {
             }
               
         }
-    } catch (error) {
-        console.error('erro : ', error);
-    }
 }
 
-// MONTANDO O SELECT
+// MONTANDO O SELECT BUSCANDO VALOR DOS PREÇOS NO BANCO
 async function montaSelect() {
-    try {
-        const response = await fetch('http://localhost:3000/precos');
-        const data = await response.json();
+    let data = await consultaApi('precos');
 
-        for (let i = 0; i < data[0].length; i++) {
-            $('#valorJogo').append(`<option value='${data[0][i].price}'>${data[0][i].price}</option>`);
-        }
+    for (let i = 0; i < data[0].length; i++) {
+        $('#valorJogo').append(`<option value='${data[0][i].price}'>${data[0][i].price}</option>`);
+    }       
+}
+
+// CONSULTA A API JUNTAO A URL (LOCALHOST:3000) COM O CAMINHO DESEJADO
+async function consultaApi(caminho) {
+    try {
+        const response = await fetch(apiUrl + caminho);
+        const data = await response.json();
+        return data;
 
     } catch (error) {
         console.error('erro', error);
     }
 }
-
 
 export {buscaTabela, pegaTop5, pegaLancamentos, pegaFiltro, montaSelect};
